d69f877f1b7c4b92304ca486c4755a54e21583fb
Eugene Kwon <ekwon86@gmail.com>
2017-11-09 09:22:32 -0800

    trying to deploy to heroku again

--- index.js
+++ index.js
@@ -36,9 +36,9 @@ app.use('/authentication', authentication);
 app.use('/events', events);
 
 // /** Connect server to Angular 2 index.html **/
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname + '/public/index.html'));
-// });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
 
 /** Start Server: listen on port 8080 **/
 app.listen(port, () => {
